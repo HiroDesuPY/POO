546ca0c46dc47c94fa592189e859f74bc3cca176
Hiro <takahashi.rafael06@gmail.com>
2026-04-01 21:59:54 -0300

    Add Primeira_Classe to manage personal information and birthday logic

---
+++ Aula 5/teste/teste.py
@@ -0,0 +1,31 @@
+class Primeira_Classe:
+    """
+    Essa classe identifica uma pessoa, com nome, idade, cidade e data de aniversário. Ela tem um método para fazer aniversário, aumentando a idade em 1, e um método para mostrar as informações da pessoa.
+    """
+    def __init__(self):
+       self.nome = ""
+       self.idade = 0
+       self.cidade = ""
+       self.aniver = ""
+
+    def aniversario (self):
+        self.idade += 1
+
+    def conclusao (self):
+        print (f'{self.nome} de cidade {self.cidade} tem {self.idade} anos de idade')
+
+
+
+data = "06/12"
+p1 = Primeira_Classe()
+p1.nome = input("Digite o nome: ")
+p1.cidade = input("Digite a cidade: ")
+p1.idade = int(input("Digite a idade: "))
+p1.aniver = input("Digite a data de aniversário: ")
+
+if p1.aniver == data:
+    p1.aniversario()
+
+p1.conclusao()
+
+print (Primeira_Classe.__doc__)
